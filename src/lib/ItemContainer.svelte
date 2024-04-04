bb6be828ecacc7ad09c7ade0646d001dd08f16c3
Francis Louie Antonio <francis.antonio0927@gmail.com>
2024-04-04 23:14:51 +0800

    Remove cursor-pointer in items

--- src/lib/ItemContainer.svelte
+++ src/lib/ItemContainer.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex gap-3 justify-center">
     {#each {length: 10} as _, index }
-        <div class={`${questionNumber >= index + 1? 'bg-linearColor' : 'bg-lightViolet'} rounded-full w-10 h-10 text-lightGray flex items-center justify-center cursor-pointer`}>   
+        <div class={`${questionNumber >= index + 1? 'bg-linearColor' : 'bg-lightViolet'} rounded-full w-10 h-10 text-lightGray flex items-center justify-center`}>   
             {index+1}
         </div>
     {/each}
