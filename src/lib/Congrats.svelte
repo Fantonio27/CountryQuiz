23cc2eff34f04d24f6c74351a2b32dd3ced45581
Francis Louie Antonio <francis.antonio0927@gmail.com>
2024-04-04 23:12:40 +0800

    Add cursor pointer to button

--- src/lib/Congrats.svelte
+++ src/lib/Congrats.svelte
@@ -6,7 +6,7 @@
     <img src={Ccngrats} alt="congrats.svg" class=" object-contain w-full"/>
     <h1 class=" text-2xl text-center my-4">Congrats! You completed <br/>the quiz.</h1>
     <p class=" text-center">You answer 4/10 correctly</p>
-    <button class="bg-linearColor px-20 py-4 rounded-xl m-auto block text-white mt-10">Play again</button>
+    <button class="bg-linearColor px-20 py-4 rounded-xl m-auto block text-white mt-10 cursor-auto">Play again</button>
 </div>
 
 
